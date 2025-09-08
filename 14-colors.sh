2e4d2d5657ae9cbf90e423df76823ca18a7a56f6
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-09-08 12:29:28 +0530

    enhanced script with colors

---
+++ 14-colors.sh
@@ -0,0 +1,55 @@
+#!/bin/bash 
+
+userid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $userid -ne 0 ]
+then 
+    echo -e "$R You are not running with root user access $N"
+    exit 1
+else 
+    echo -e "$G You are running with root user access $N"
+fi 
+
+VALIDATE()
+{
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$G $1 is installed successfully $N"
+    else 
+        echo -e "$R $1 not installed successfully $N"
+        exit 1 
+    fi
+}
+
+dnf list installed mysql 
+if [ $? -ne 0 ]
+then 
+    echo -e "$R MySql is not there we are going to install!!!"
+    dnf install mysql -y 
+    VALIDATE $? "MySql"
+else 
+    echo -e "$Y MySql is already there we are skipping $N"
+fi 
+
+dnf list installed nginx 
+if [ $? -ne 0 ]
+then 
+    echo -e "$R Nginx is not there we are going to install $N"
+    VALIDATE $? "Nginx"
+else 
+    echo -e "$Y Nginx is already there we are skipping $N"
+fi 
+
+dnf list installed python 
+if [ $? -ne 0 ]
+then 
+    echo -e "$R Python is not here we are going to install!!! $N"
+    dnf install python -y
+    VALIDATE $? "Python"
+else 
+    echo -e "$Y Python is already there we are skipping $N"
