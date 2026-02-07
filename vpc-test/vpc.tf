23ec2f1176e162f62bd1dcf0dda8cc7c5af96db3
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2026-02-08 00:59:06 +0530

    deleted all previous content

--- vpc-test/vpc.tf
+++
@@ -1,9 +0,0 @@
-module "vpc" {
-    source = "git::https://github.com/manojkumar-tech267/practice.git//vpc?ref=main"
-    project = "roboshop"
-    environment = "dev"
-    public_cidr_blocks = ["10.0.1.0/24","10.0.2.0/24"]
-    private_cidr_blocks = ["10.0.11.0/24","10.0.12.0/24"]
-    database_cidr_blocks = ["10.0.21.0/24","10.0.22.0/24"]
-    is_peering_required = true
-}
--- vpc/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-    common_tags = {
-        Project = var.project
-        Environment = var.environment
-        Terraform = true
-    }
-    az_info = slice(data.aws_availability_zones.available.names,0,2)
-}
