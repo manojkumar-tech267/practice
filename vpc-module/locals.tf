c1affb5d6b55b586508702f10889fe60072c4cfa
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2026-04-26 23:03:05 +0530

    practice vpc module updated

--- vpc-module/locals.tf
+++ vpc-module/locals.tf
@@ -18,4 +18,5 @@ locals {
         },
         var.igw_tags
     )
+    az_info = slice(data.aws_availability_zones.available.names,0,2)
 }
--- vpc-module/main.tf
+++ vpc-module/main.tf
@@ -14,13 +14,56 @@ resource "aws_internet_gateway" "main" {
 }
 
 
-# resource "aws_subnet" "public" {
-#   count = length(var.public_subnet_cidrs)  
-#   vpc_id     = aws_vpc.main.id
-#   cidr_block = var.public_subnet_cidrs[count.index]
-#   availability_zone = ""
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)  
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = local.az_info[count.index]
+  map_public_ip_on_launch = true
 
-#   tags = {
-#     Name = "Main"
-#   }
-# }
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-public-${local.az_info[count.index]}"
+    },
+    var.public_subnet_tags
+  )
+}
+
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = local.az_info[count.index]
+  map_public_ip_on_launch = false
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-private-${local.az_info[count.index]}"
+    },
+    var.private_subnet_tags
+  )
+}
+
+
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+  availability_zone = local.az_info[count.index]
+  map_public_ip_on_launch = false
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-database-${local.az_info[count.index]}"
+    },
+    var.database_subnet_tags
+  )
+}
+
+
+
+
