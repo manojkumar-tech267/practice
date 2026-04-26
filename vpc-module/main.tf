eae733fc8e4c079522ae39416f1294b8f4d74efb
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2026-04-26 23:38:45 +0530

    vpc module updated

--- vpc-module/main.tf
+++ vpc-module/main.tf
@@ -110,6 +110,17 @@ resource "aws_nat_gateway" "main" {
   depends_on = [aws_internet_gateway.main]
 }
 
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
+
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
 
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnet_cidrs)
@@ -125,7 +136,7 @@ resource "aws_route_table_association" "private" {
 
 
 resource "aws_route_table_association" "database" {
-  count = length(var.database_subnet_tags)
+  count = length(var.database_subnet_cidrs)
   subnet_id      = aws_subnet.database[count.index].id
   route_table_id = aws_route_table.database.id
 }
