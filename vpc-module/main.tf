33ab7c8d8ef4064b1b20c78f0364a2432934f5fd
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2026-04-26 22:41:00 +0530

    vpc practice module

---
+++ vpc-module/main.tf
@@ -0,0 +1,26 @@
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr_block
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = local.vpc_final_tags
+}
+
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = local.igw_final_tags
+}
+
+
+# resource "aws_subnet" "public" {
+#   count = length(var.public_subnet_cidrs)  
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = var.public_subnet_cidrs[count.index]
+#   availability_zone = ""
+
+#   tags = {
+#     Name = "Main"
+#   }
+# }
